bdc049d6248c7c1e84640f9eeb4ba4beb46d8c4a
yunnasheng <yunnasheng@payeasenet.com>
2020-12-31 13:41:41 +0800

    dubbo配置zk注册中心

---
+++ springboot-dubbo-zk-api/src/main/java/com/lb/springboot/service/UserService.java
@@ -0,0 +1,13 @@
+package com.lb.springboot.service;
+
+/**
+ * 功能描述:UserService <br/>
+ *
+ * @author yunnasheng
+ * @date: 2020-12-31 13:13<br/>
+ * @since JDK 1.8
+ */
+public interface UserService {
+
+    String get(String id);
+}
--- springboot-dubbo-zk-provider/src/main/java/com/lb/springboot/service/impl/GreetingServiceImpl.java
+++ springboot-dubbo-zk-provider/src/main/java/com/lb/springboot/service/impl/GreetingServiceImpl.java
@@ -2,6 +2,10 @@ package com.lb.springboot.service.impl;
 
 import com.lb.springboot.service.GreetingService;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
 
 /**
  * 功能描述:GreetingServiceImpl <br/>
@@ -10,11 +14,25 @@ import org.apache.dubbo.config.annotation.DubboService;
  * @date: 2020-12-30 17:14<br/>
  * @since JDK 1.8
  */
-@DubboService(version = "1.0",registry = "${dubbo.registry.server}")
+@DubboService(version = "1.0")
 public class GreetingServiceImpl implements GreetingService {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Random costTimeRandom = new Random();
 
     @Override
     public String sayHi(String name) {
+        await();
         return String.format("Hi, %s",name);
+    }
+
+    private void await() {
+        try {
+            long timeInMillisToWait = costTimeRandom.nextInt(500);
+            Thread.sleep(timeInMillisToWait);
+            logger.info("execution time : " + timeInMillisToWait + " ms.");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
